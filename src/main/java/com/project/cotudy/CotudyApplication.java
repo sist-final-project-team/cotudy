f7c7f6ab6deeb977b2175d0c6fc46c936d110845
catsrang <davin325@gmail.com>
2019-10-16 14:31:17 +0900

    임시

--- src/main/java/com/project/cotudy/CotudyApplication.java
+++ src/main/java/com/project/cotudy/CotudyApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CotudyApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(CotudyApplication.class, args);
     }
 
--- src/main/java/com/project/cotudy/controller/TestController.java
+++ src/main/java/com/project/cotudy/controller/TestController.java
@@ -1,5 +1,5 @@
 package com.project.cotudy.controller;
 
 public class TestController {
-    test;
+
 }
