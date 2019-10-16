0e7f25e3a1390d9975c396a5ee305dd7df718658
sklim0932 <sklim0932@gmail.com>
2019-10-16 12:29:59 +0900

    first commit - init project

---
+++ src/main/java/com/project/cotudy/CotudyApplication.java
@@ -0,0 +1,13 @@
+package com.project.cotudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CotudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CotudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/cotudy/CotudyApplicationTests.java
@@ -0,0 +1,16 @@
+package com.project.cotudy;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CotudyApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
