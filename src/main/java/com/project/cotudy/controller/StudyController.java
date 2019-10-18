2a24f452508bca1d740ed5f184b007b906d0d857
catsrang <davin325@gmail.com>
2019-10-18 11:41:22 +0900

    update database configuration

--- src/main/java/com/project/cotudy/controller/StudyController.java
+++ src/main/java/com/project/cotudy/controller/StudyController.java
@@ -8,82 +8,82 @@ public class StudyController {
 
     @RequestMapping("/")
     public String main() {
-        return "main";
+        return "/main";
     }
 
     @RequestMapping("/event")
     public String event() {
-        return "event";
+        return "/event";
     }
 
     @RequestMapping("/login")
     public String login() {
-        return "login";
+        return "/login";
     }
 
     @RequestMapping("/join")
     public String join() {
-        return "join";
+        return "/join";
     }
 
     @RequestMapping("/notice")
     public String notice() {
-        return "notice";
+        return "/notice";
     }
 
     @RequestMapping("/freeCont")
     public String freeBoardCont() {
-        return "freeBoardCont";
+        return "/freeBoardCont";
     }
 
     @RequestMapping("/freeEdit")
     public String freeBoardEdit() {
-        return "freeBoardEdit";
+        return "/freeBoardEdit";
     }
 
     @RequestMapping("/freeList")
     public String freeBoardList() {
-        return "freeBoardList";
+        return "/freeBoardList";
     }
 
     @RequestMapping("/freeWrite")
     public String freeBoardWrite() {
-        return "freeBoardWrite";
+        return "/freeBoardWrite";
     }
 
     @RequestMapping("/studyCont")
     public String studyBoardCont() {
-        return "studyBoardCont";
+        return "/studyBoardCont";
     }
 
     @RequestMapping("/studyCreate")
     public String studyBoardCreate() {
-        return "studyBoardCreate";
+        return "/studyBoardCreate";
     }
 
     @RequestMapping("/bookMark")
     public String memBookMark() {
-        return "memBookMark";
+        return "/memBookMark";
     }
 
     @RequestMapping("/infoEdit")
     public String memInfoEdit() {
-        return "memInfoEdit";
+        return "/memInfoEdit";
     }
 
     @RequestMapping("/myWrite")
     public String memMyWrite() {
-        return "memMyWrite";
+        return "/memMyWrite";
     }
 
     @RequestMapping("/out")
     public String memOut() {
-        return "memOut";
+        return "/memOut";
     }
 
     @RequestMapping("/myPage")
     public String memPage() {
-        return "memPage";
+        return "/memPage";
     }
 
 }
